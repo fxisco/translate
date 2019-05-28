471019f00fb789502f80a7cac6f2899dd27b5404
Francisco Payés <payesfrancisco@gmail.com>
2019-05-28 19:18:16 -0400

    feat: translate input

--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
+	"strings"
+
+	"cloud.google.com/go/translate"
+	"golang.org/x/text/language"
 )
 
 func main() {
-	source := flag.String("src", "en", "Source language to use for translation.")
 	dest := flag.String("dest", "es", "Destination language for translation.")
 
 	flag.Parse()
 
-	for _, value := range flag.Args() {
-		fmt.Println(value)
+	sentence := strings.Join(flag.Args(), " ")
+
+	fmt.Println(*dest, sentence)
+
+	ctx := context.Background()
+	client, err := translate.NewClient(ctx)
+
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	fmt.Printf("%v %v", *source, *dest)
+	target, err := language.Parse(*dest)
+
+	if err != nil {
+		log.Fatalf("Failed to parse target language: %v", err)
+	}
+
+	translations, err := client.Translate(ctx, []string{sentence}, target, nil)
+
+	if err != nil {
+		log.Fatalf("Failed to translate text: %v", err)
+	}
+
+	fmt.Printf("Translation: %v\n", translations[0].Text)
 }
